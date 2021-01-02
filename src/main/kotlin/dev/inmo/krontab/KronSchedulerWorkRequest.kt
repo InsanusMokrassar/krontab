0376eb791037977394e05f94a1cfce5d03042e35
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-01-02 22:43:32 +0600

    add android target

---
+++ src/main/kotlin/dev/inmo/krontab/KronSchedulerWorkRequest.kt
@@ -0,0 +1,18 @@
+package dev.inmo.krontab
+
+import android.content.Context
+import androidx.work.*
+
+//fun KronScheduler.workRequest(context: Context) = OneTimeWorkRequestBuilder<>()
+//
+//abstract class KronSchedulerWorkRequest(
+//    context: Context,
+//    workerParams: WorkerParameters
+//) : CoroutineWorker(
+//    context,
+//    workerParams
+//) {
+//    override suspend fun doWork(): Result {
+//
+//    }
+//}
